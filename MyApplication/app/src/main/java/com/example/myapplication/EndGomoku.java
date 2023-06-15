7fed6a3592646912c6a89981b6c1c6e4de24ee6d
yuanningliu <liuyuanning142857@gmail.com>
2023-06-15 10:58:06 -0400

    added end screen for gomoku

---
+++ MyApplication/app/src/main/java/com/example/myapplication/EndGomoku.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EndGomoku extends AppCompatActivity{
+    private TextView player1Name;
+    private ImageView player1Avatar;
+    private TextView player2Name;
+    private ImageView player2Avatar;
+    private Button playAgain, quitGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.end_gomoku);
+
+        player1Name = findViewById(R.id.player1Name);
+        player1Name.setText(InitialConfigGomoku.userName1);
+        player2Name = findViewById(R.id.player2Name);
+        player2Name.setText(InitialConfigGomoku.userName2);
+
+        player1Avatar = findViewById(R.id.player1Avatar);
+        player1Avatar.setImageDrawable(InitialConfigGomoku.player_avatar1.getDrawable());
+        player2Avatar = findViewById(R.id.player2Avatar);
+        player2Avatar.setImageDrawable(InitialConfigGomoku.player_avatar2.getDrawable());
+
+        playAgain = findViewById(R.id.playAgain);
+        quitGame = findViewById(R.id.quitGame);
+
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EndGomoku.this, Gomoku.class);
+                startActivity(intent);
+            }
+        });
+        quitGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EndGomoku.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
