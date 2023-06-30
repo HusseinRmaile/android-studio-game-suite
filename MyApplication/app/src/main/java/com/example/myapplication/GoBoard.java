d6f8c7a5b2aca335f38a72e8d5e9d194b0b86572
tateJohnson <tjohnson400@gatech.edu>
2023-06-29 23:32:18 -0400

    stone objects implemented

--- MyApplication/app/src/main/java/com/example/myapplication/GoBoard.java
+++ MyApplication/app/src/main/java/com/example/myapplication/GoBoard.java
@@ -7,23 +7,23 @@ import java.util.Stack;
 public class GoBoard {
     private int numRows;
     private int numCols;
-    private int board[][];
+    private GoStone board[][];
     //rows and cols corresponds to placeable areas
     //not grid, but rather the corners the stones can go on
     private int surroundLen;
     private int spacesLeft;
 
-    private Stack<Integer> delList = new Stack<>();
+    private Stack<GoStone> delList = new Stack<>();
 
     //default 19x19 board with 5 in a row win condition
     public GoBoard() {
         this.numRows = 9;
         this.numCols = 9;
         this.spacesLeft = numRows * numCols;
-        this.board = new int[numRows][numCols];
+        this.board = new GoStone[numRows][numCols];
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                board[i][j] = 0;
+                board[i][j] = null;
             }
         }
     }
@@ -33,23 +33,29 @@ public class GoBoard {
         this.numRows = height;
         this.numCols = width;
         this.spacesLeft = numRows * numCols;
-        this.board = new int[numRows][numCols];
+        this.board = new GoStone[numRows][numCols];
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                board[i][j] = 0;
+                board[i][j] = null;
             }
         }
     }
 
     public ArrayList<Integer> placePiece(int row, int col, int playerNumber){
-        if(board[row][col] != 0){
+        if(board[row][col] != null){
             return null;
             //a playerNumber is already there
         }
-        board[row][col] = playerNumber;
+        GoStone Cur;
+        if (playerNumber == 1) {
+            Cur = new GoBlackStone(row,col);
+        } else {
+            Cur = new GoWhiteStone(row,col);
+        }
+        board[row][col] = Cur;
         ArrayList<Integer> ret = capture(row, col, playerNumber % 2 + 1);
         if(libertyCount(row, col, playerNumber, true) == 0){
-            board[row][col] = 0;
+            board[row][col] = null;
             unCheck();
             delList.clear();
             return null;
@@ -68,15 +74,16 @@ public class GoBoard {
         if (row < 0 || row == numRows || col < 0 || col == numCols) {
             return 0;
         }
-        int color = board[row][col];
-        if (color != playerNumber && !puttingDown) {
-            if (color == 0) {
-                return 1;
-            }
+        if (board[row][col] == null && !puttingDown) {
+            return 1;
+        }
+        GoStone marked = board[row][col];
+        int color = marked.getColor();
+        if (color != playerNumber || marked.isChain()) {
             return 0;
         }
-        board[row][col] += 3;
-        delList.push(row * 9 + col);
+        marked.setChain(true);
+        delList.push(marked);
         liberty += libertyCount(row - 1,col,playerNumber, false);
         liberty += libertyCount(row + 1,col,playerNumber, false);
         liberty += libertyCount(row,col - 1,playerNumber, false);
@@ -89,9 +96,9 @@ public class GoBoard {
         if (libertyCount(row - 1,col,playerNumber, false) == 0) {
             int size = delList.size();
             for (int i = 0; i < size; i++) {
-                int index = delList.pop();
-                board[index / 9][index % 9] -= 3;
-                trueDel.add(index);
+                GoStone Cur = delList.pop();
+                Cur.setChain(false);
+                trueDel.add(Cur.getIndex());
             }
         } else {
             unCheck();
@@ -100,9 +107,9 @@ public class GoBoard {
         if (libertyCount(row + 1,col,playerNumber, false) == 0) {
             int size = delList.size();
             for (int i = 0; i < size; i++) {
-                int index = delList.pop();
-                board[index / 9][index % 9] -= 3;
-                trueDel.add(index);
+                GoStone Cur = delList.pop();
+                Cur.setChain(false);
+                trueDel.add(Cur.getIndex());
             }
         } else {
             unCheck();
@@ -111,9 +118,9 @@ public class GoBoard {
         if (libertyCount(row,col - 1,playerNumber, false) == 0) {
             int size = delList.size();
             for (int i = 0; i < size; i++) {
-                int index = delList.pop();
-                board[index / 9][index % 9] -= 3;
-                trueDel.add(index);
+                GoStone Cur = delList.pop();
+                Cur.setChain(false);
+                trueDel.add(Cur.getIndex());
             }
         } else {
             unCheck();
@@ -122,9 +129,9 @@ public class GoBoard {
         if (libertyCount(row,col + 1,playerNumber, false) == 0) {
             int size = delList.size();
             for (int i = 0; i < size; i++) {
-                int index = delList.pop();
-                board[index / 9][index % 9] -= 3;
-                trueDel.add(index);
+                GoStone Cur = delList.pop();
+                Cur.setChain(false);
+                trueDel.add(Cur.getIndex());
             }
         } else {
             unCheck();
@@ -132,7 +139,7 @@ public class GoBoard {
         delList.clear();
         for (int i = 0; i < trueDel.size(); i++) {
             int index = trueDel.get(i);
-            board[index / 9][index % 9] = 0;
+            board[index / 9][index % 9] = null;
         }
         printBoard();
         return trueDel;
@@ -143,12 +150,16 @@ public class GoBoard {
     }
 
     public void printBoard() {
-
         //Loop through the board printing one character at a time
         for(int i=0; i<numRows; ++i) {
             for(int j=0; j<numCols; ++j) {
+                GoStone a = board[i][j];
                 System.out.print('[');
-                System.out.print(board[i][j]);
+                if (a == null) {
+                    System.out.print(0);
+                } else {
+                    System.out.print(board[i][j].getColor());
+                }
                 System.out.print("] ");
             }
             System.out.print('\n');
@@ -156,9 +167,9 @@ public class GoBoard {
     }
 
     private void unCheck() {
-        for (int i:
+        for (GoStone i:
              delList) {
-            board[i / 9][i % 9] -= 3;
+            i.setChain(false);
         }
     }
 
