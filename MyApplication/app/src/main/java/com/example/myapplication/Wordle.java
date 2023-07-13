f5015b7777ba7ca86faf6adf36ace1bdf579f207
Akkawi, Zaid Kayed <zakkawi3@gatech.edu>
2023-07-13 10:57:20 -0400

    - made a qwerty ordered array

--- MyApplication/app/src/main/java/com/example/myapplication/Wordle.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Wordle.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Wordle extends AppCompatActivity{
     private TextView nameDisplay;
     private ImageView dynamicImageView;
+    private char[] qwerty = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,4 +23,5 @@ public class Wordle extends AppCompatActivity{
         dynamicImageView = findViewById(R.id.player1Avatar);
         dynamicImageView.setImageDrawable(InitialConfigWordle.avatar.getDrawable());
     }
+
 }
