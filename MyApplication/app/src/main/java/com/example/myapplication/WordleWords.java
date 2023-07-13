e00cf80397ae00835eb8bc040016552654749d6c
Taiki Aiba <taiba3@gatech.edu>
2023-07-13 12:48:25 -0400

    made methods for adding words and initializing the word list with the default words in WordleWords.java -Taiki

--- MyApplication/app/src/main/java/com/example/myapplication/WordleWords.java
+++ MyApplication/app/src/main/java/com/example/myapplication/WordleWords.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
-
+import java.util.ArrayList;
 public class WordleWords {
     private volatile static WordleWords words;
+    private ArrayList<String> wordList;
+    private boolean defaultInitialized;
     private WordleWords(){};
 
     public static WordleWords getInstance(){
@@ -13,5 +15,29 @@ public class WordleWords {
             }
         }
         return words;
+    }
+
+    public void addWords(String words){
+        defaultWords();
+        wordList.add(wordList.size(), words);
+    }
+    public void addWords(ArrayList<String> words) {
+        defaultWords();
+        for (int i = 0; i < words.size(); i++) {
+            wordList.add(wordList.size(), words.get(i));
+        }
+    }
+
+    private void defaultWords() {
+        if (defaultInitialized) {
+            return;
+        }
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Phone");
+        words.add("Lunch");
+        words.add("Arrow");
+        words.add("Whirl");
+
+        wordList = words;
     }
 }
